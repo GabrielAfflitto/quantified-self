e8f54b23492b199c7effc9fb4039f079ccbec379
GabrielAfflitto <gafflitto92@gmail.com>
2018-03-15 16:56:32 -0600

    added link on homepage to foods.html

--- main.bundle.js
+++ main.bundle.js
@@ -44,7 +44,74 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	"use strict";
+	'use strict';
+
+	var getAllFoods = function getAllFoods() {
+	  $('#all-foods').html('<tr><th>Food</th><th>Calories</th></tr>');
+	  fetch('https://quantifiedself-backend.herokuapp.com/api/v1/foods').then(function (response) {
+	    return response.json();
+	  }).then(function (foods) {
+	    return getEachFood(foods);
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	var getEachFood = function getEachFood(foods) {
+	  var sortedFoods = foods.sort(function (a, b) {
+	    if (a.id > b.id) {
+	      return -1;
+	    } else {
+	      return 1;
+	    }
+	  });
+	  sortedFoods.forEach(function (food) {
+	    $('#all-foods').append('<tr>\n        <td>' + food.name + '</td>\n        <td>' + food.calories + '</td>\n        <td><button id="' + food.id + '" class="delete-btn" aria-label="Delete">\n          Delete\n        </button></td>\n      </tr>');
+	  });
+	};
+
+	var removeFood = function removeFood(event) {
+	  event.target.parentNode.parentNode.remove();
+	  deleteFood(event.target.id);
+	};
+
+	var deleteFood = function deleteFood(id) {
+	  fetch('https://quantifiedself-backend.herokuapp.com/api/v1/foods/' + id, { method: 'DELETE' }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	var createFood = function createFood(event) {
+	  event.preventDefault();
+	  var name = $('#name').val();
+	  var calories = $('#calories').val();
+
+	  postFood({ name: name, calories: calories });
+	};
+
+	var postConfig = function postConfig(body) {
+	  return {
+	    method: 'POST',
+	    headers: { 'Content-Type': 'application/json' },
+	    body: JSON.stringify(body)
+	  };
+	};
+
+	var postFood = function postFood(body) {
+	  fetch('https://quantifiedself-backend.herokuapp.com/api/v1/foods', postConfig(body)).then(function (response) {
+	    return response.json();
+	  }).then(function () {
+	    return getAllFoods();
+	  }).catch(function (error) {
+	    return console.error({ error: error });
+	  });
+	};
+
+	getAllFoods();
+
+	$('#add-food-btn').on('click', createFood);
+
+	$('#all-foods').on('click', '.delete-btn', removeFood);
 
 /***/ })
 /******/ ]);
